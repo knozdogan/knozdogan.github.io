0d683bcf97b27089b5b175c6d550ca02db5b2e03
Kaan Ozdogan <knozdogan@gmail.com>
2019-05-06 14:01:48 +0300

    touch fixing

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 function setup() {
-  createCanvas(600,600);
-  stroke(0, 102);
+  createCanvas(400,600);
+  //stroke(0, 102);
 }
 
 function draw() {
   //background(0);
-  var weight = dist(touchX, touchY, ptouchX, ptouchY);
-  strokeWeight(weight);
-  line(touchX, touchY, ptouchX, ptouchY);
+  //var weight = dist(touchX, touchY, ptouchX, ptouchY);
+  //strokeWeight(weight);
+  //line(touchX, touchY, ptouchX, ptouchY);
+  ellipse(touchX, touchY, 20, 20);
 }
