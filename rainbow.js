bc85339427445b394f85541286214bb8e24d28a9
Kaan Ozdogan <knozdogan@gmail.com>
2021-09-12 00:35:00 +0300

    updated as rainbow artwork

---
+++ rainbow.js
@@ -0,0 +1,19 @@
+var hue;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(0);
+	hue=0;
+}
+
+function draw() {
+  if (hue > 360) {
+    hue = 0;
+  } else {
+    hue++;
+  }
+  colorMode(HSL, 360);
+  noStroke();
+  fill(hue, 200, 200);
+  ellipse(mouseX, mouseY, 20, 20);
+}
