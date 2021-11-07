a4d3b62fa02ec1035ff2ee1068cba3d044ace962
Kaan Özdoğan <knozdogan@gmail.com>
2021-11-08 01:17:31 +0300

    fixed num of rectangles

--- artworks/pattern/pattern.js
+++ artworks/pattern/pattern.js
@@ -1,37 +1,37 @@
-var theta = 0.0;
+var theta = Date.now()%360;
 var rotationSpeed = 0.001;
-var multiplier = 5;
-var _hue = 0;
+var multiplier = 10;
+//var _hue = 0;
 var start_time=Date.now();
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   rectMode(CENTER);
-  colorMode(HSB,360,100,100);
+  //colorMode(HSB,360,100,100);
 }
 
 function draw() {
-  background(0);
+  background(255);
   noFill();
 
-  //stationary rect
+  // rect-1
   push();
   translate(width/2, height/2);
-  rotate(- theta + 180);
-  for(var i =0; i < width; i++){
-    stroke(_hue%360,100,100)
+  rotate(- theta + 90);
+  for(var i =0; i < width * 70/height; i++){
+    //stroke(_hue%360,100,100)
     rect(0, 0,
     i*multiplier,
     i*multiplier);
   }
   pop();
 
-  //rotating rect
+  // rect-2
   push();
   translate(width/2, height/2);
   rotate(theta);
-  for(var i =0; i < width; i++){
-    stroke((_hue%360+180)%360,100,100)
+  for(var i =0; i < width * 70/height; i++){
+    //stroke((_hue%360+180)%360,100,100)
     rect(0, 0,
     i*multiplier,
     i*multiplier);
@@ -39,10 +39,10 @@ function draw() {
   pop();
   theta += rotationSpeed;
 
-  if(Date.now()-start_time > 1000){
-    _hue += 10;
-    print(_hue%360);
-    start_time = Date.now();
-  }
+  // if(Date.now()-start_time > 1000){
+  //   _hue += 10;
+  //   print(_hue%360);
+  //   start_time = Date.now();
+  // }
 
 }
